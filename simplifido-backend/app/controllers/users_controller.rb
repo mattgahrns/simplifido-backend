edc9da4b847d2ed196c836c7859f9ef8798a2d7c
Matt Gahrns <matthewgahrns@hotmail.com>
2019-12-30 14:48:23 -0800

    Implemented basic auth on the backend with JWT

--- simplifido-backend/app/controllers/users_controller.rb
+++ simplifido-backend/app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
+
+    def profile
+        render json: { user: current_user) }, status: :accepted
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            render json: { user: @user}, status: :created
+        else
+            render json: { error: 'failed to create user'}, status: :not_acceptable
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :password_digest, :email, :city, :state, :img_url)
+    end
 end
