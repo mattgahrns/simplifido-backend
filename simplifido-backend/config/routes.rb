86327267d2afb9c7256604e5799b84f465aeb143
Matt Gahrns <matthewgahrns@hotmail.com>
2019-12-31 10:20:37 -0800

    Added a route for signup and got signup working

--- simplifido-backend/config/routes.rb
+++ simplifido-backend/config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   resources :comments
   resources :posts
   resources :dogs
-  resources :users
+  resources :users, only: [:index, :new, :show, :edit, :update, :destroy]
   post '/login', to: 'auth#create'
   get '/profile', to: 'users#profile'
+  post '/signup', to: 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
